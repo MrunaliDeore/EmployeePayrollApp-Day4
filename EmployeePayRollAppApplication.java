74ddf814b3bbe4026a7694a6bb2596b4006c0502
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-08-31 13:56:52 +0530

    using Annotation @Slf4j at Class Level, Logging is activated

---
+++ EmployeePayRollAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayRollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayRollAppApplication.class, args);
+        log.info("Employee Payroll App Stated...!");
+    }
+
+}
